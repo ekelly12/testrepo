03d253517cfb6ea5fb5e682d58d16e4ac9534cc5
Eric Kelly <erickelly@microsoft.com>
2021-12-02 13:29:57 -0800

    adding isOutput=true

--- signals/setup.sh
+++ signals/setup.sh
@@ -4,9 +4,9 @@ export BUILD_ROOT=/home/ekelly/ek
 export BUILD_LIB="$BUILD_ROOT/signals"
 export BUILD_SOURCE="$BUILD_ROOT/signals"
 
-echo "##vso[task.setvariable variable=BUILD_ROOT]$BUILD_ROOT"
-echo "##vso[task.setvariable variable=BUILD_LIB]$BUILD_LIB"
-echo "##vso[task.setvariable variable=BUILD_SOURCE]$BUILD_SOURCE"
+echo "##vso[task.setvariable variable=BUILD_ROOT;isOutput=true]$BUILD_ROOT"
+echo "##vso[task.setvariable variable=BUILD_LIB;isOutput=true]$BUILD_LIB"
+echo "##vso[task.setvariable variable=BUILD_SOURCE;isOutput=true]$BUILD_SOURCE"
 
 ulimit -c unlimited \
 && sudo sysctl -w kernel.core_pattern=/tmp/core/%e-%p-%s-%u.core
