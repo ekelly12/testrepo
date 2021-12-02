2fc3f2b2ea2f13a4c154a2789e9bbe1909fa013b
Eric Kelly <erickelly@microsoft.com>
2021-12-02 10:37:56 -0800

    adding ##vso logging cmd for env vars

--- signals/setup.sh
+++ signals/setup.sh
@@ -4,5 +4,9 @@ export BUILD_ROOT=/home/ekelly/ek
 export BUILD_LIB="$BUILD_ROOT/signals"
 export BUILD_SOURCE="$BUILD_ROOT/signals"
 
+echo "##vso[task.setvariable variable=BUILD_ROOT]$BUILD_ROOT"
+echo "##vso[task.setvariable variable=BUILD_LIB]$BUILD_LIB"
+echo "##vso[task.setvariable variable=BUILD_SOURCE]$BUILD_SOURCE"
+
 ulimit -c unlimited \
 && sudo sysctl -w kernel.core_pattern=/tmp/core/%e-%p-%s-%u.core
