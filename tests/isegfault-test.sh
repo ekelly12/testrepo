7f940335f02aa99e91902c4aa0322963311d2b92
Eric Kelly <erickelly@microsoft.com>
2021-12-05 10:34:26 -0800

    display of soft limit

--- tests/isegfault-test.sh
+++ tests/isegfault-test.sh
@@ -3,6 +3,9 @@
 # Set core dump file size limit.
 ulimit -Sc 99999999
 
+coreFileSize=$(ulimit -Sc)
+echo "Core dump file size (soft)limit set to $coreFileSize)"
+
 # Run isegfault and captire the exit code.
 returnCode=$($BUILD_ROOT/out/isegfault || echo $?)
 
@@ -12,6 +15,6 @@ then
 	echo "isegfault expectedly returned an exit code of 139."
 else
 	echo "isegfault returned an exist code other than 139."
-	echo "exit code returned: $returnCode"
+	echo "exit code returned: '$returnCode'"
 	exit 1
 fi
