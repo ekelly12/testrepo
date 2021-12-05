810a246cc2a8b22060859072c1bc00d4e57efc73
Eric Kelly <erickelly@microsoft.com>
2021-12-05 10:43:20 -0800

    paths and .gitingore

--- isegfault/build.sh
+++ isegfault/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-COMPILE_STR="g++ -g -I$BUILD_ROOT/isegfault -o \"$BUILD_OUT_DIR/isegfault\" \"$BUILD_ROOT/isegfault/isegfault.cpp\""
+COMPILE_STR="g++ -g -I./isegfault -o \"./isegfault/isegfault\" \"./isegfault/isegfault.cpp\""
 echo $COMPILE_STR
 eval $COMPILE_STR
