ccfa7d48d39e82e54a718db54f4415fa14cd8ba9
Eric Kelly <erickelly@microsoft.com>
2021-12-16 22:38:05 -0800

    what am I doing wrong? this works

--- script/CaptureCritical/setup.sh
+++ script/CaptureCritical/setup.sh
@@ -3,11 +3,11 @@
 
 # NOTE: This script must be called as root or via sudo.
 
-if ((EUID != 0)); then
-  echo "Please run as root - current Effective UID = ${EUID}"
-  id
-  exit 1
-fi
+#if ((EUID != 0)); then
+#  echo "Please run as root - current Effective UID = ${EUID}"
+#  id
+#  exit 1
+#fi
 
 # Check for required parameters.
 # First parameter, BUILD_ROOT.
